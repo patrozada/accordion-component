510f5c62fd9d02271170e829d2378099fd502fd0
patrozada <patriciarozada@gmail.com>
2020-08-28 10:14:08 +0200

    Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
+import { allCategories } from './App';
 
-test('renders learn react link', () => {
+
+test('renders app title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = getByText(/Servicios disponibles/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+describe ('Accordion', () => {
+
+  it('should have as many children as services available', () => {
+    const testCategories = [
+      {"Id": "1", "Caption": "a"},
+      {"Id": "2", "Caption": "b"},
+      {"Id": "3", "Caption": "c"}
+    ]
+    render(<App />);
+
+    testCategories.map(item => screen.findByDisplayValue(item.Caption))
+  })
 });
