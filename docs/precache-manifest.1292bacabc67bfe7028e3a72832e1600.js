bb9cdb24d7017b54cb59568bfb6c00ae7c2d69c6
patrozada <patriciarozada@gmail.com>
2020-08-31 17:54:06 +0200

    Create build

---
+++ docs/precache-manifest.1292bacabc67bfe7028e3a72832e1600.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800ce49bdb929202f6befdf824a14892",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be4219c6d3f607aca617",
+    "url": "./static/css/main.cf36bc89.chunk.css"
+  },
+  {
+    "revision": "b822354a81f74e0cf0cc",
+    "url": "./static/js/2.f29c5a07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f29c5a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4219c6d3f607aca617",
+    "url": "./static/js/main.e9a79062.chunk.js"
+  },
+  {
+    "revision": "a1d516e0fc95fed3afcf",
+    "url": "./static/js/runtime-main.595f225d.js"
+  }
+]);
